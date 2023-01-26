34f30fd2d06ace6162289bc3b828b70f3ef43e4d
syzgod <viktor.berczeli@gmail.com>
2023-01-26 23:29:52 +0100

    Design change and simplify multiple aspects

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import axios from 'axios';
 import React, { useState, useEffect, useRef } from 'react';
 import { useGeolocated } from 'react-geolocated';
+import cloud from './assets/icons/2995000_cloud_weather_cloudy_rain_sun_icon.png';
+import sun from './assets/icons/1530392_weather_sun_sunny_temperature_icon.png';
 
 function App() {
   const ref = useRef<HTMLInputElement>(null);
@@ -9,6 +11,14 @@ function App() {
   const [loading, setLoading] = useState(true);
   const [lat, setLat] = useState<number>();
   const [long, setLong] = useState<number>();
+  const [checked, setChecked] = React.useState(false);
+
+  const date = new Date();
+
+  let day = date.getDate();
+  let month = date.getMonth() + 1;
+  let year = date.getFullYear();
+  let currentDate = `${day}-${month}-${year}`;
 
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${process.env.REACT_APP_OPENWEATHERMAP_API_KEY}&units=metric`;
 
@@ -30,6 +40,10 @@ function App() {
   const locationHandle = (e: any): any => {
     e.preventDefault();
     setLocation(ref.current!.value);
+  };
+
+  const handleChange = () => {
+    setChecked(!checked);
   };
 
   const { coords, isGeolocationAvailable, isGeolocationEnabled } =
@@ -65,9 +79,9 @@ function App() {
   };
 
   return (
-    <div className='flex min-h-screen flex-col items-center justify-center bg-gray-800'>
-      <div className='rounded-3xl border-2 border-gray-900 bg-gray-700 p-6 text-gray-200 shadow-xl shadow-gray-900'>
-        <h1 className='text mb-4 items-center justify-center border-b-2 border-green-600 pb-4 text-center text-4xl font-bold'>
+    <div className='flex min-h-screen flex-col items-center justify-center bg-gradient-to-r from-sky-800 to-slate-900'>
+      <div className='rounded-3xl border-2 border-gray-900 bg-transparent p-6 text-gray-200 shadow-xl shadow-gray-900 backdrop-blur-md'>
+        <h1 className='text mb-4 items-center justify-center border-b-2 border-gray-200 pb-4 text-center text-4xl font-bold'>
           My Weather App
         </h1>
         {!isGeolocationAvailable ? (
@@ -75,97 +89,131 @@ function App() {
         ) : !isGeolocationEnabled ? (
           <div>Geolocation is not enabled</div>
         ) : coords && !loading ? (
-          <>
-            <table>
-              <tbody>
-                <tr>
-                  <td>latitude</td>
-                  <td>{coords.latitude}</td>
-                </tr>
-                <tr>
-                  <td>longitude</td>
-                  <td>{coords.longitude}</td>
-                </tr>
-
-                {/*<tr>
-          <td>altitude</td>
-          <td>{coords.altitude}</td>
-        </tr>
-        <tr>
-          <td>heading</td>
-          <td>{coords.heading}</td>
-        </tr>
-        <tr>
-          <td>speed</td>
-          <td>{coords.speed}</td>
-  </tr>*/}
-              </tbody>
-            </table>
+          <div className='mb-4 flex flex-col items-center justify-center'>
+            <div className='mb-3 flex w-fit flex-col items-center justify-center rounded-xl border-2 border-gray-300 bg-slate-400 bg-opacity-20 p-3'>
+              <div>Current location coords: </div>
+              <table>
+                <tbody>
+                  <tr className='flex flex-col'>
+                    <td className='font-bold'>Latitude: </td>
+                    <td>{coords.latitude}</td>
+                  </tr>
+                  <tr className='flex flex-col'>
+                    <td className='font-bold'>Longitude: </td>
+                    <td>{coords.longitude}</td>
+                  </tr>
+                  {/*<tr>
+                        <td>altitude</td>
+                        <td>{coords.altitude}</td>
+                      </tr>
+                      <tr>
+                        <td>heading</td>
+                        <td>{coords.heading}</td>
+                      </tr>
+                      <tr>
+                        <td>speed</td>
+                        <td>{coords.speed}</td>
+                </tr>*/}
+                </tbody>
+              </table>
+            </div>
             <button
               onClick={() => getPlaceHandle(coords.latitude, coords.longitude)}
-              className='ml-3 rounded-3xl border p-2 hover:bg-gray-300 hover:text-gray-700'
+              className='h-12 rounded-full border-2 border-gray-300 bg-slate-400 bg-opacity-20 p-2 text-center hover:bg-opacity-50'
             >
               Get my location
             </button>
-          </>
+          </div>
         ) : (
           <div>Getting the location data&hellip; </div>
         )}
-        <form onSubmit={locationHandle}>
-          <div className='flex items-center justify-center text-lg'>
-            <input
-              ref={ref}
-              className='m-2 w-fit rounded-2xl border-2 border-gray-600 bg-gray-400 p-2 text-gray-700 placeholder-gray-700 focus:outline-none focus:ring focus:ring-green-600'
-              type='text'
-              name=''
-              id=''
-              placeholder='Enter location name'
-            />
-            <button
-              className='h-12 rounded-full border-2 border-gray-600 p-2 text-center hover:bg-green-600 hover:text-gray-700 hover:shadow-inner hover:shadow-gray-700'
-              type='submit'
-            >
-              Get location
-            </button>
+        <hr />
+        <form
+          onSubmit={locationHandle}
+          className='mt-4 flex flex-col items-center justify-center'
+        >
+          <div className='flex flex-col items-center justify-center text-lg'>
+            <div>
+              <input
+                ref={ref}
+                className='m-2 w-fit rounded-2xl border-2 border-gray-300 bg-slate-400 bg-opacity-20 p-2 text-gray-200 placeholder-gray-400 focus:outline-none focus:ring focus:ring-gray-300'
+                type='text'
+                name=''
+                id=''
+                placeholder='Enter location name'
+              />
+              <button
+                className='h-12 rounded-full border-2 border-gray-300 bg-slate-400 bg-opacity-20 p-2 text-center hover:bg-opacity-50'
+                type='submit'
+              >
+                Get location
+              </button>
+            </div>
+            <div>
+              <input
+                checked={checked}
+                onChange={handleChange}
+                type='checkbox'
+                name=''
+                id='wind'
+              />
+              <label className='mx-3' htmlFor='wind'>
+                Wind
+              </label>
+              <input
+                checked={checked}
+                onChange={handleChange}
+                type='checkbox'
+                name=''
+                id='wind_direction'
+              />
+              <label className='ml-2' htmlFor='wind_direction'>
+                Wind direction
+              </label>
+            </div>
           </div>
           {loading && <div>Loading...</div>}
           {!loading ? (
             <>
-              <div className='mt-4 rounded-3xl border border-b-8 border-l-8 border-green-600 bg-gray-600 p-6 text-lg leading-relaxed'>
-                <h1>
-                  Current weather in{' '}
-                  <span className='font-bold'>
-                    {weatherData.name}, {weatherData.sys.country}
-                  </span>
+              <div className='mt-4 flex w-2/3 flex-col items-center justify-center rounded-3xl border border-b-8 border-r-8 border-slate-800 bg-slate-400 bg-opacity-20 p-6 text-lg leading-relaxed'>
+                {weatherData.weather[0].description.includes('cloud') ? (
+                  <img className='h-16 w-16' src={cloud} alt='' />
+                ) : null}
+                {weatherData.weather[0].description.includes('sun') ||
+                weatherData.weather[0].description.includes('clear') ? (
+                  <img className='h-16 w-16' src={sun} alt='' />
+                ) : null}
+                <h1 className='font-bold'>
+                  {weatherData.name}, {weatherData.sys.country}
                 </h1>
+                <h3 className=''>{currentDate}</h3>
+
+                <h2 className=''>Current weather</h2>
+
                 <p>
-                  The actual temperature is:{' '}
                   <span className='font-bold'>
                     {Math.trunc(weatherData.main.temp)}
                   </span>{' '}
-                  which feels like{' '}
+                  feels like{' '}
                   <span className='font-bold'>
                     {Math.trunc(weatherData.main.feels_like)}
                   </span>
                 </p>
                 <p>
-                  The highest temperature is:{' '}
+                  highest{' '}
                   <span className='font-bold'>
                     {Math.trunc(weatherData.main.temp_max)}
                   </span>
                 </p>
                 <p>
-                  The lowest temperature is:{' '}
-                  <span className='font-bold'>
+                  lowest{' '}
+                  <span className='font-bold text-gray-200'>
                     {Math.trunc(weatherData.main.temp_min)}
                   </span>
                 </p>
-                <p>
-                  Current conditions:{' '}
-                  <span className='font-bold'>
-                    {weatherData.weather[0].description}
-                  </span>
-                </p>
+                <div className='font-bold'>
+                  {weatherData.weather[0].description}
+                </div>
               </div>
             </>
           ) : null}
