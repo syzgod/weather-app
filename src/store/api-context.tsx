0cbb8e2b4dacad611e945d73ba9f609254ace15b
syzgod <viktor.berczeli@gmail.com>
2023-01-31 22:41:33 +0100

    Prepare global state usage for API and Checkboxes

---
+++ src/store/api-context.tsx
@@ -0,0 +1,5 @@
+import { createContext } from 'react';
+
+const ApiContext = createContext<any>(null);
+
+export default ApiContext;
---
+++ src/store/checkbox-context.tsx
@@ -0,0 +1,5 @@
+import { createContext } from 'react';
+
+const CheckboxContext = createContext<any>([]);
+
+export default CheckboxContext;
