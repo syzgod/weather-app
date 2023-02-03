467a13f20c25e467e578b8f1e219398a4a149826
Viktor Berczeli <86021488+syzgod@users.noreply.github.com>
2023-02-03 22:12:53 +0100

    Add wind direction

--- src/components/WeatherCard.tsx
+++ src/components/WeatherCard.tsx
@@ -22,64 +22,64 @@ const WeatherCard = ({ weatherData }: any) => {
   const iconURL = 'http://openweathermap.org/img/wn/';
   // let icon = `${apiCtx.weather[0].icon}.png`;
 
-  // let deg;
-  // const getDirection = async() => {
-  //   await deg = Math.floor(apiCtx.wind.deg)
-  // }
+  let deg;
+  const getDirection = async () => {
+    deg = await Math.floor(apiCtx.wind.deg);
+    switch (true) {
+      case deg >= 360 && deg <= 21:
+        deg = 'N';
+        break;
+      case deg >= 22 && deg <= 44:
+        deg = 'NNE';
+        break;
+      case deg >= 45 && deg <= 66:
+        deg = 'NE';
+        break;
+      case deg >= 67 && deg <= 89:
+        deg = 'ENE';
+        break;
+      case deg >= 90 && deg <= 111:
+        deg = 'E';
+        break;
+      case deg >= 112 && deg <= 134:
+        deg = 'ESE';
+        break;
+      case deg >= 135 && deg <= 156:
+        deg = 'SE';
+        break;
+      case deg >= 157 && deg <= 179:
+        deg = 'SSE';
+        break;
+      case deg >= 180 && deg <= 201:
+        deg = 'S';
+        break;
+      case deg >= 202 && deg <= 224:
+        deg = 'SSW';
+        break;
+      case deg >= 225 && deg <= 246:
+        deg = 'SW';
+        break;
+      case deg >= 247 && deg <= 269:
+        deg = 'WSW';
+        break;
+      case deg >= 270 && deg <= 291:
+        deg = 'W';
+        break;
+      case deg >= 292 && deg <= 314:
+        deg = 'WNW';
+        break;
+      case deg >= 315 && deg <= 336:
+        deg = 'NW';
+        break;
+      case deg >= 337 && deg <= 359:
+        deg = 'NNW';
+        break;
+      default:
+        deg = 'no data';
+    }
+    return deg;
+  };
   // console.log(deg);
-
-  // switch (true) {
-  //   case deg >= 360 && deg <= 21:
-  //     deg = 'N';
-  //     break;
-  //   case deg >= 22 && deg <= 44:
-  //     deg = 'NNE';
-  //     break;
-  //   case deg >= 45 && deg <= 66:
-  //     deg = 'NE';
-  //     break;
-  //   case deg >= 67 && deg <= 89:
-  //     deg = 'ENE';
-  //     break;
-  //   case deg >= 90 && deg <= 111:
-  //     deg = 'E';
-  //     break;
-  //   case deg >= 112 && deg <= 134:
-  //     deg = 'ESE';
-  //     break;
-  //   case deg >= 135 && deg <= 156:
-  //     deg = 'SE';
-  //     break;
-  //   case deg >= 157 && deg <= 179:
-  //     deg = 'SSE';
-  //     break;
-  //   case deg >= 180 && deg <= 201:
-  //     deg = 'S';
-  //     break;
-  //   case deg >= 202 && deg <= 224:
-  //     deg = 'SSW';
-  //     break;
-  //   case deg >= 225 && deg <= 246:
-  //     deg = 'SW';
-  //     break;
-  //   case deg >= 247 && deg <= 269:
-  //     deg = 'WSW';
-  //     break;
-  //   case deg >= 270 && deg <= 291:
-  //     deg = 'W';
-  //     break;
-  //   case deg >= 292 && deg <= 314:
-  //     deg = 'WNW';
-  //     break;
-  //   case deg >= 315 && deg <= 336:
-  //     deg = 'NW';
-  //     break;
-  //   case deg >= 337 && deg <= 359:
-  //     deg = 'NNW';
-  //     break;
-  //   default:
-  //     deg = 'no data';
-  // }
 
   return apiCtx.weather?.length ? (
     <div className='mt-4 flex w-2/3 flex-col items-center justify-center rounded-3xl border border-b-8 border-r-8 border-slate-800 bg-slate-400 bg-opacity-20 p-6 text-lg leading-relaxed'>
@@ -116,7 +116,7 @@ const WeatherCard = ({ weatherData }: any) => {
       )}
       {state.checkboxes.map((checkbox) =>
         checkbox.value === 'windDirection' && checkbox.checked ? (
-          <div>{apiCtx.wind.deg}</div>
+          <div>{getDirection()}</div>
         ) : null
       )}
       {state.checkboxes.map((checkbox) =>
